9321fa9c177980b1f4b696619f59131cf18ff3f5
soongu <naong526@naver.com>
2024-05-20 09:58:12 +0900

    spring mvc - 게시판 앱 2

--- src/main/java/com/study/springstudy/springmvc/chap04/controller/BoardController.java
+++ src/main/java/com/study/springstudy/springmvc/chap04/controller/BoardController.java
@@ -1,14 +1,47 @@
 package com.study.springstudy.springmvc.chap04.controller;
 
+import com.study.springstudy.springmvc.chap04.dto.BoardWriteRequestDto;
+import com.study.springstudy.springmvc.chap04.entity.Board;
+import com.study.springstudy.springmvc.chap04.repository.BoardRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/board")
+@RequiredArgsConstructor
 public class BoardController {
+
+    private final BoardRepository repository;
 
     // 1. 목록 조회 요청 (/board/list : GET)
 
-
     // 2. 게시글 쓰기 양식 화면 열기 요청 (/board/write : GET)
+    @GetMapping("/write")
+    public String write() {
+        System.out.println("/board/write GET! ");
+        return "board/write";
+    }
 
     // 3. 게시글 등록 요청 (/board/write : POST)
     // -> 목록조회 요청 리다이렉션
+    @PostMapping("/write")
+    public String write(BoardWriteRequestDto dto) {
+        System.out.println("/board/write POST! ");
+
+        // 1. 브라우저가 전달한 게시글 내용 읽기
+        System.out.println("dto = " + dto);
+
+        // 2. 해당 게시글을 데이터베이스에 저장하기 위해 엔터티 클래스로 변환
+        Board b = dto.toEntity();
+
+        // 3. 데이터베이스 저장 명령
+        repository.save(b);
+
+        return "";
+    }
 
     // 4. 게시글 삭제 요청 (/board/delete : GET)
     // -> 목록조회 요청 리다이렉션
