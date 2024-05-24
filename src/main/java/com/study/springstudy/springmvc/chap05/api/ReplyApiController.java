455654d0dfbf6fe8f7fe46b085cde3c5f4a634b3
soongu <naong526@naver.com>
2024-05-24 14:33:27 +0900

    댓글 API 등록기능

--- src/main/java/com/study/springstudy/springmvc/chap05/api/ReplyApiController.java
+++ src/main/java/com/study/springstudy/springmvc/chap05/api/ReplyApiController.java
@@ -1,15 +1,13 @@
 package com.study.springstudy.springmvc.chap05.api;
 
+import com.study.springstudy.springmvc.chap05.dto.request.ReplyPostDto;
 import com.study.springstudy.springmvc.chap05.dto.response.ReplyDetailDto;
 import com.study.springstudy.springmvc.chap05.entity.Reply;
 import com.study.springstudy.springmvc.chap05.service.ReplyService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -43,6 +41,25 @@ public class ReplyApiController {
         return ResponseEntity
                 .ok()
                 .body(replies);
+    }
+
+    // 댓글 생성 요청
+    // @RequestBody : 클라이언트가 전송한 데이터를 JSON으로 받아서 파싱
+    @PostMapping
+    public ResponseEntity<?> posts(@RequestBody ReplyPostDto dto) {
+
+        log.info("/api/v1/replies : POST");
+        log.debug("parameter: {}", dto);
+
+        boolean flag = replyService.register(dto);
+
+        if (!flag) return ResponseEntity
+                .internalServerError()
+                .body("댓글 등록 실패!");
+
+        return ResponseEntity
+                .ok()
+                .body(replyService.getReplies(dto.getBno()));
     }
 
 }
