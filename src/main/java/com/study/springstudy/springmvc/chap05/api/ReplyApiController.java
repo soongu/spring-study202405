79b44428f34b39ce6951897b7efa009c1f1a00d6
soongu <naong526@naver.com>
2024-06-04 14:57:57 +0900

    # 댓글 인가처리 - 댓글쓰기 기능 수정

--- src/main/java/com/study/springstudy/springmvc/chap05/api/ReplyApiController.java
+++ src/main/java/com/study/springstudy/springmvc/chap05/api/ReplyApiController.java
@@ -14,6 +14,7 @@ import org.springframework.validation.FieldError;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpSession;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -58,6 +59,7 @@ public class ReplyApiController {
     public ResponseEntity<?> posts(
             @Validated @RequestBody ReplyPostDto dto
             , BindingResult result // 입력값 검증 결과 데이터를 갖고 있는 객체
+            , HttpSession session
     ) {
         log.info("/api/v1/replies : POST");
         log.debug("parameter: {}", dto);
@@ -70,7 +72,7 @@ public class ReplyApiController {
                     .body(errors);
         }
 
-        boolean flag = replyService.register(dto);
+        boolean flag = replyService.register(dto, session);
 
         if (!flag) return ResponseEntity
                 .internalServerError()
