5da6212a38ecaec42b1d76aa048e59734aec244d
soongu <naong526@naver.com>
2024-06-05 12:32:31 +0900

    # 회원가입 - 프로필사진 처리 4

--- src/main/java/com/study/springstudy/springmvc/chap05/dto/response/LoginUserInfoDto.java
+++ src/main/java/com/study/springstudy/springmvc/chap05/dto/response/LoginUserInfoDto.java
@@ -15,12 +15,14 @@ public class LoginUserInfoDto {
     private String nickName;
     private String email;
     private String auth;
+    private String profile;
 
     public LoginUserInfoDto(Member member) {
         this.account = member.getAccount();
         this.email = member.getEmail();
         this.nickName = member.getName();
         this.auth = member.getAuth().name();
+        this.profile = member.getProfileImg();
     }
 
 }
--- src/main/java/com/study/springstudy/springmvc/util/FileUtil.java
+++ src/main/java/com/study/springstudy/springmvc/util/FileUtil.java
@@ -47,6 +47,7 @@ public class FileUtil {
         // url-path: /local/2024/06/05/djlfsjdjsf_dog.png
         String urlPath = "/local"+fullPath.substring(rootPath.length());
 
+        System.out.println("urlPath = " + urlPath);
         // 업로드가 완료되면 데이터베이스에 파일의 경로 위치를 저장
         // ex) /local/2024/06/05/dkfjsldjfkslfjlds_dog.jpg
         return urlPath;
