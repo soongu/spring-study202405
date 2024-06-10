0c62acf774ad4b7301e615d9db430d13bd26569b
soongu <naong526@naver.com>
2024-06-10 11:32:51 +0900

    # 카카오 로그인 후속처리

--- src/main/java/com/study/springstudy/springmvc/chap05/api/SnsLoginController.java
+++ src/main/java/com/study/springstudy/springmvc/chap05/api/SnsLoginController.java
@@ -1,15 +1,13 @@
 package com.study.springstudy.springmvc.chap05.api;
 
 import com.study.springstudy.springmvc.chap05.service.SnsLoginService;
-import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.servlet.http.HttpSession;
 import java.util.HashMap;
 
 @Controller
@@ -39,7 +37,7 @@ public class SnsLoginController {
 
     // 인가코드를 받는 요청 메서드
     @GetMapping("/oauth/kakao")
-    public String kakaoCode(String code) {
+    public String kakaoCode(String code, HttpSession session) {
         log.info("카카오 인가코드 발급 - {}", code);
 
         // 토큰 발급에 필요한 파라미터 만들기
@@ -49,9 +47,9 @@ public class SnsLoginController {
         requestParams.put("code", code);
 
         // 인증 액세스 토큰 발급 요청
-        snsLoginService.kakaoLogin(requestParams);
+        snsLoginService.kakaoLogin(requestParams, session);
 
-        return "";
+        return "redirect:/";
     }
 
 
--- src/main/java/com/study/springstudy/springmvc/chap05/service/SnsLoginService.java
+++ src/main/java/com/study/springstudy/springmvc/chap05/service/SnsLoginService.java
@@ -1,6 +1,10 @@
 package com.study.springstudy.springmvc.chap05.service;
 
+import com.study.springstudy.springmvc.chap05.dto.request.SignUpDto;
 import com.study.springstudy.springmvc.chap05.dto.response.AccessTokenDto;
+import com.study.springstudy.springmvc.chap05.dto.response.KaKaoUserDto;
+import com.study.springstudy.springmvc.chap05.mapper.MemberMapper;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
@@ -10,27 +14,52 @@ import org.springframework.stereotype.Service;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.HashMap;
+import javax.servlet.http.HttpSession;
 import java.util.Map;
 
 @Service
 @Slf4j
+@RequiredArgsConstructor
 public class SnsLoginService {
 
+    private final MemberService memberService;
+    private final MemberMapper memberMapper;
+
     // 카카오 로그인 처리 서비스 로직
-    public void kakaoLogin(Map<String, Object> requestParams) {
+    public void kakaoLogin(Map<String, Object> requestParams, HttpSession session) {
 
         // 토큰 발급 요청
         String accessToken = getKakaoAccessToken(requestParams);
 
         // 발급받은 토큰으로 사용자 정보 가져오기
-        getKakaoUserInfo(accessToken);
+        KaKaoUserDto.Properties userInfo = getKakaoUserInfo(accessToken);
 
+        // 카카오에서 받은 회원정보로 우리 사이트 회원가입 시키기
+        String account = userInfo.getNickname();
+        // 회원 중복확인
+        if (memberService.checkIdentifier("account", account)) {
+            throw new IllegalArgumentException("이미 가입한 회원입니다.");
+        }
+
+        memberService.join(
+                SignUpDto.builder()
+                        .account(account)
+                        .password("0000")
+                        .name(account)
+                        .email(account + "@abc.com")
+                        .build(),
+                userInfo.getProfileImage()
+        );
+
+        // 우리 사이트 로그인 처리
+        MemberService.maintainLoginState(session, memberMapper.findOne(account));
+        // 세션에 현재 로그인 방식에 대해 지정
+        session.setAttribute("loginPath", "kakao");
 
     }
 
     // 토큰으로 사용자 정보 요청
-    private void getKakaoUserInfo(String accessToken) {
+    private KaKaoUserDto.Properties getKakaoUserInfo(String accessToken) {
 
         // request uri
         String requestUri = "https://kapi.kakao.com/v2/user/me";
@@ -43,17 +72,18 @@ public class SnsLoginService {
 
         // 요청 보내기
         RestTemplate template = new RestTemplate();
-        ResponseEntity<Map> response = template.exchange(
+        ResponseEntity<KaKaoUserDto> response = template.exchange(
                 requestUri
                 , HttpMethod.POST
                 , new HttpEntity<>(headers)
-                , Map.class
+                , KaKaoUserDto.class
         );
 
         // 응답 정보 JSON 꺼내기
-        Map json = response.getBody();
+        KaKaoUserDto json = response.getBody();
         log.debug("user profile: {}", json);
 
+        return json.getProperties();
     }
 
     // 인가코드로 토큰 발급 요청
